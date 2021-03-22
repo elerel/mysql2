83e320859679316a0d268cc85a79bc862ac320c2
elerel <elcranley@gmail.com>
2021-03-22 22:08:35 +0000

    completed delete many tutorial

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,23 +1,22 @@
 import os
+import datetime
 import pymysql
 
-# Get username from Cloud9 workspace
+# Get the username from the Cloud9 workspace
 # (modify this variable if running on another environment)
-
 username = os.getenv('C9_USER')
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
-                            user=username,
-                            password='',
-                            db='Chinook')
+                             user=username,
+                             password='',
+                             db='Chinook')
 
 try:
-    # run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        cursor.execute("DELETE FROM Friends WHERE name in ('jim', 'bob')")
+        connection.commit()
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
 finally:
     connection.close()
